50ca2647ad1f17e91e7356262ac6e86d3a7dd3ae
nur2144214 <nurnasyrdyn@gmail.com>
2025-10-19 22:11:44 +0600

    hw4

---
+++ hw4.py
@@ -0,0 +1,19 @@
+# git clone https://github.com/Alb1ngid/26_bot.git
+# cd 26_bot
+# git remote remove origin
+# git remote add origin https://github.com/YOUR_USERNAME/YOUR_NEW_REPO.git
+# echo ".idea/\nvenv/\n__pycache__/\n*.pyc\n.env" > .gitignore
+# git add .gitignore
+# git commit -m "Add .gitignore"
+# python -m venv venv
+# # Windows:
+# venv\Scripts\activate
+# # Linux / macOS:
+# source venv/bin/activate
+# pip install -r reg.txt
+# pip freeze > requirements.txt
+# git add .
+# git commit -m "Initial import, install deps, add requirements"
+# git branch -M main
+# git push -u origin main
+# deactivate
